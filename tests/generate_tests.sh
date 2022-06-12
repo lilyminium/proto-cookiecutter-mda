8652b308736363794a05a8a2d40dfb5a9153a706
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2022-06-12 16:51:23 +1000

    no poetry

--- tests/generate_tests.sh
+++ tests/generate_tests.sh
@@ -2,13 +2,14 @@
 
 # Regenerate the reference workflow scripts
 
+cd ~/workspace
 rm -rf test_mda*rtd
-./setup_cookiecutter.py --project "Test Project with PyPI and ReadTheDocs" --repo test_mda_pip_rtd --account lilyminium --dependencies 3 --rtd 1 --cookiecutter ..
-./setup_cookiecutter.py --project "Test Project with Conda-Forge and ReadTheDocs" --repo test_mda_condaforge_rtd --account lilyminium --dependencies 1 --rtd 1 --cookiecutter ..
-./setup_cookiecutter.py --project "Test Project with Anaconda and no ReadTheDocs" --repo test_mda_conda_nortd --account lilyminium --dependencies 2 --rtd 2 --cookiecutter ..
+/Users/lily/pydev/cookiecutter-mdanalysis/tests/setup_cookiecutter.py --project "Test Project with PyPI and ReadTheDocs" --repo test_mda_pip_rtd --account lilyminium --dependencies 3 --rtd 1 --cookiecutter "/Users/lily/pydev/cookiecutter-mdanalysis"
+/Users/lily/pydev/cookiecutter-mdanalysis/tests/setup_cookiecutter.py --project "Test Project with Conda-Forge and ReadTheDocs" --repo test_mda_condaforge_rtd --account lilyminium --dependencies 1 --rtd 1 --cookiecutter "/Users/lily/pydev/cookiecutter-mdanalysis"
+/Users/lily/pydev/cookiecutter-mdanalysis/tests/setup_cookiecutter.py --project "Test Project with Anaconda and no ReadTheDocs" --repo test_mda_conda_nortd --account lilyminium --dependencies 2 --rtd 2 --cookiecutter "/Users/lily/pydev/cookiecutter-mdanalysis"
 
 for fname in test_mda_conda_nortd test_mda_condaforge_rtd test_mda_pip_rtd ; do
-	cd ${fname}
+	cd  ${fname}
 	git remote add origin git@github.com:lilyminium/${fname}.git
 	git push -f origin main
 	cd -
--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup, find_packages
 import versioneer
 
-short_description = __doc__.split("\n")
+short_description = "{{cookiecutter.description}}".strip().split("\n")[0]
 
 # from https://github.com/pytest-dev/pytest-runner#conditional-requirement
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
@@ -24,12 +24,12 @@ setup(
     name='{{cookiecutter.repo_name}}',
     author='{{cookiecutter.author_name}}',
     author_email='{{cookiecutter.author_email}}',
-    description=short_description[0],
+    description=short_description,
     long_description=long_description,
     long_description_content_type="text/markdown",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    license='GNU Public License v3',
+    license='GNU Public License v2+',
 
     # Which Python importable modules should be included when your package is installed
     # Handled automatically by setuptools. Use 'exclude' to prevent some specific
@@ -40,10 +40,11 @@ setup(
     # Customize MANIFEST.in if the general case does not suit your needs
     # Comment out this line to prevent the files from being packaged with your software
     include_package_data=True,
-
+    python_requires=">=3.8",          # Python version restrictions
     # Allows `setup.py test` to work correctly with pytest
     setup_requires=[] + pytest_runner,
-    # Required packages, pulls from pip if needed; do not use for Conda deployment
+    # Required packages, pulls from pip if needed
+    # do not use for Conda deployment
     install_requires=[
         "mdanalysis",
     ],
@@ -53,9 +54,19 @@ setup(
     #            'Mac OS-X',
     #            'Unix',
     #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
-    # python_requires=">=3.8",          # Python version restrictions
 
     # Manual control if final package is compressible or not, set False to prevent the .egg from being made
     # zip_safe=False,
 
+    extras_require={
+        "test": [
+            "pytest>=6.0",
+            "pytest-xdist>=2.5",
+            "pytest-cov>=3.0",
+        ],
+        "doc": [
+            "sphinx>=1.8",
+            "sphinx_rtd_theme>=1.0",
+        ]
+    }
 )
